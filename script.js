b23f815edd390563987389ee24dd45ebc68ab3df
BlankIsaac4345 <isaacml4345@gmail.com>
2026-02-17 18:26:51 +0000

    updaqte 1.0

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Script started")
+
+function startExample() {
+    let numBoxes = prompt("How many boxes?");
+    let numBadges = prompt("How many badges?");
+    let numhours = prompt("How many hours?")
+}
