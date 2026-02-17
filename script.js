2b0652c949ede5945cd77fee2cfcc656621b4a74
BlankIsaac4345 <isaacml4345@gmail.com>
2026-02-17 18:32:56 +0000

    update 2.0

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ function startExample() {
     let numBoxes = prompt("How many boxes?");
     let numBadges = prompt("How many badges?");
     let numhours = prompt("How many hours?")
+
+    let isGold = numBoxes >= 100 || numBoxez >= 50 && numBadges >= 10 && numHours >= 25;
+
+    alert("is gold status:" + isGold);
 }
